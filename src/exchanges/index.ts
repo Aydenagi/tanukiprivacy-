88da4aabc82f499b47b62673d12003e8d0e8f1a6
Ayden <diamondkaz578@gmail.com>
2025-12-23 20:24:22 -0500

    Delete src/exchanges directory

--- src/exchanges/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Quote, TradeParams, TradeResult } from '../types.js';
-
-export interface IExchange {
-  readonly network: 'solana' | 'bsc' | 'astar';
-  getQuote(params: TradeParams): Promise<Quote>;
-  swap(params: TradeParams, route?: any): Promise<TradeResult>;
-  getBalance?(symbolOrAddress: string): Promise<string>;
-}
